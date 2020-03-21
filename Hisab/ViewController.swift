82be36d6c77bd8569d84e21a1d3291b08127dd11
IrfanNawawi <irfnawi@gmail.com>
2020-03-21 18:33:40 +0700

    First Commit

--- Hisab/ViewController.swift
+++ Hisab/ViewController.swift
@@ -10,11 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelNumberShow: UILabel!
+    
+    //properties
+    var isNewOperation = true
+    var selectedOp = "+"
+    var number1: Double?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func actionNumberTapped(_ sender: Any) {
+        let selectedButton = sender as! UIButton
+        let tapedNumber = selectedButton.titleLabel?.text
+        addNumberToInput(number: tapedNumber!)
+    }
+    
+    
+    @IBAction func actionOpTapped(_ sender: Any) {
+        let buttonOp = sender as! UIButton
+        selectedOp = buttonOp.titleLabel?.text ?? "+"
+        number1 = Double(labelNumberShow.text!)
+        isNewOperation = true
+    }
+    
+    @IBAction func actionEqualsTapped(_ sender: Any) {
+        let number2 = Double(labelNumberShow.text!)
+        var results: Double?
+        switch selectedOp {
+        case "+":
+            results = number1! + number2!
+        case "-":
+            results = number1! - number2!
+        case "/":
+            results = number1! / number2!
+        case "x":
+            results = number1! * number2!
+        default:
+            results = 0.0
+        }
+        
+        labelNumberShow.text = String(results!)
+        isNewOperation = true
+    }
+    
+    @IBAction func actionResetTapped(_ sender: Any) {
+        isNewOperation = true
+        labelNumberShow.text = "0"
+        number1 = 0
+    }
+    
+    func addNumberToInput(number: String) {
+        var textNumber = String(labelNumberShow.text!)
+        if isNewOperation {
+            textNumber = ""
+            isNewOperation = false
+        }
+        textNumber = textNumber + number
+        labelNumberShow.text = textNumber
+    }
+    
 }
 
